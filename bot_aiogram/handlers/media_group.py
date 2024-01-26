42e2ddead005aba3e99c7c77d4c8e269d236d9d2
Konstantin Gashev <gashev1989@yandex.ru>
2024-01-27 00:48:23 +0500

    Update media_group.py

--- bot_aiogram/handlers/media_group.py
+++ bot_aiogram/handlers/media_group.py
@@ -19,7 +19,32 @@ async def get_media_group(message: Message, bot: Bot):
 
 async def watch_media_group(message: Message, bot: Bot):
     """Сеты для просмотра перед бронированием стола."""
-    message.answer(
-        'Тут должны быть сеты',
-        reply_markup=ReplyKeyboardRemove()
+    set_1 = InputMediaPhoto(
+        type='photo', media=FSInputFile(r'images/set1.jpg'),
     )
+    set_2 = InputMediaPhoto(
+        type='photo', media=FSInputFile(r'images/set2.jpg')
+    )
+    set_3 = InputMediaPhoto(
+        type='photo', media=FSInputFile(r'images/set3.jpg')
+    )
+    set_4 = InputMediaPhoto(
+        type='photo', media=FSInputFile(r'images/set4.jpg')
+    )
+    set_5 = InputMediaPhoto(
+        type='photo', media=FSInputFile(r'images/set5.jpg')
+    )
+    set_6 = InputMediaPhoto(
+        type='photo', media=FSInputFile(r'images/set6.jpg')
+    )
+    set_7 = InputMediaPhoto(
+        type='photo', media=FSInputFile(r'images/set7.jpg')
+    )
+    set_8 = InputMediaPhoto(
+        type='photo', media=FSInputFile(r'images/set8.jpg')
+    )
+    set_9 = InputMediaPhoto(
+        type='photo', media=FSInputFile(r'images/set9.jpg')
+    )
+    media = [set_1, set_2, set_3, set_4, set_5, set_6, set_7, set_8, set_9]
+    await message.answer_media_group(media)
